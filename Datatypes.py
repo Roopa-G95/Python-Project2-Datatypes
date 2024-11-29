66386d4d30e9e6057a3d315fcfd8fd98eac45fa0
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-11-29 14:16:57 +0530

    Datatypes

---
+++ Datatypes.py
@@ -0,0 +1,30 @@
+a = 10
+b = 2.45
+c = -34.6  #Float with - symbol
+d = -87.7e100 #Number with alphabet
+e = 2 + 3j
+f = -5j
+g = 'Python data types'
+h = 12E3   #Number with alphabet
+_ab = 24   #Variable name starting with '_'
+k = True
+l = 'Python\nsession' #Print in next line
+m = 'Python\tsession' #print with tab space
+o = float(a)
+p = int(d)
+#q = float(e) ---> complex number & mix of numbers_string cannot be changed to float or int
+
+print(type(a),'a')
+print(type(b),'b')
+print(type(c),'c')
+print(type(d),'d')
+print(type(e),'e')
+print(type(f),'f')
+print(type(g),'g')
+print(type(h),'h')
+print(_ab,type(h),'_ab')
+print(type(k),'k')
+print(l)
+print(m)
+print(o)
+print(p)
